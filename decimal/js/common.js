5fc814e507cf04c6186064d669c1a0ad5638f1c1
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-03 15:38:21 +0900

    Update common.js

--- decimal/js/common.js
+++ decimal/js/common.js
@@ -1,25 +1,30 @@
 let num = prompt('1~100 사이의 숫자를 입력해 주세요');
-if( num >= 1 && num <= 100 ){
 
-    let checkPrimNum = [];
-    for (var i = 0; i < num; i++) {
-        checkPrimNum.push(i);
+if (num >= 1 && num <= 100) {
+
+  let checkPrimNum = [];
+  for (let i = 2; i <= num; i++) {
+    checkPrimNum.push(i);
+  }
+
+  let primFilter = [];
+  for (let startNum = 2; startNum <= num; startNum++) {
+    let isPrime = true;
+    for (let i = 2; i < startNum; i++) {
+      if (startNum % i === 0) {
+        isPrime = false;
+        break;
+      }
     }
-    for (let startNum = 2; startNum <= num; startNum++){
-        for (let i = startNum; i < startNum; i++) {
-                if (startNum % i === 0) {
-                    
-                } else if (startNum == i){
-                    let primFilter = checkPrimNum.filter(function (val) { return val == i });
-                }
-            }
+    if (isPrime) {
+      primFilter.push(startNum);
     }
-    primFilter.forEach(
-        function (val) {
-            alert("소수는" + val + "입니다.");
-        }
-    )
+  }
+
+  primFilter.forEach(function (val) {
+    alert("소수는 " + val + "입니다.");
+  });
 
 } else {
-    alert('잘못된 입력값 입니다.')
+  alert('잘못된 입력값 입니다.');
 }
