43a4191e2274ddfac7d3544312ab54dba6c1ae3c
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-05-03 15:59:17 +0900

    dma

--- decimal/js/common.js
+++ decimal/js/common.js
@@ -2,29 +2,41 @@ let num = prompt('1~100 사이의 숫자를 입력해 주세요');
 
 if (num >= 1 && num <= 100) {
 
-  let checkPrimNum = [];
-  for (let i = 2; i <= num; i++) {
-    checkPrimNum.push(i);
-  }
+    // let checkPrimNum = [];
+    // for (let i = 1; i <= num; i++) {
+    //     checkPrimNum.push(i);
+    //     for (let startNum = 2; startNum < i; startNum++) {
+    //         if (startNum % i === 0) {
+    //             
+    //         }
+    //         if(startNum == i) {
+    //         resultPrime = checkPrimNum.filter(startNum => startNum);
+    //             alert("소수는 " + resultPrime + "입니다.");
+    //         }
+    //     }
+    // }
 
-  let primFilter = [];
-  for (let startNum = 2; startNum <= num; startNum++) {
+    let primFilter = [];
+    for (let startNum = 2; startNum <= num; startNum++) {
     let isPrime = true;
     for (let i = 2; i < startNum; i++) {
-      if (startNum % i === 0) {
+        if (startNum % i === 0) {
         isPrime = false;
         break;
-      }
+        }
     }
     if (isPrime) {
-      primFilter.push(startNum);
+        primFilter.push(startNum);
     }
-  }
+    }
 
-  primFilter.forEach(function (val) {
-    alert("소수는 " + val + "입니다.");
-  });
+    alert("소수는 " + primFilter + "입니다.");
+
+    // primFilter.forEach(function (val) {
+    // alert("소수는 " + val + "입니다.");
+    // });
 
 } else {
-  alert('잘못된 입력값 입니다.');
+    alert('잘못된 입력값 입니다.');
 }
+
